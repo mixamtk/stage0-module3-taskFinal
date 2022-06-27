4221adcce4f2cf27723092f042efbcd3e165d3e2
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-27 12:33:37 +0300

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,6 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
-        System.out.printf("%d", number % 10 + number % 100 / 10 + number / 100);
+        System.out.printf("%d", number % 10 + number % 100 / 10 + number / 100 % 10 + number / 1000);
     }
 }
