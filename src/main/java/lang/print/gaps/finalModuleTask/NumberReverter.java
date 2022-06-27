e28a1f2dfe44b0798a3803a6039f21fd7d827ffe
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-27 12:07:56 +0300

    Update NumberReverter.java

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,6 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        System.out.printf("%d%d%d", number % 10, number % 100 / 10, number / 100 );
     }
 }
